52eed315b4e944a6d660fa5c44d0d22476e014b0
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-12-19 13:08:10 -0800

    tree

--- winter_recipes/Tree.java
+++ winter_recipes/Tree.java
@@ -19,9 +19,9 @@ public class Tree  {
 
 	  // 1. Make the robot go fast
 
-	  
+	  rob.setSpeed(100);
 	  // 2. Tell the robot to put its pen down
-
+      rob.penDown();
 	  // 3. Find the method called drawBranches below and complete its code
       drawBranches();
       
@@ -36,29 +36,30 @@ public class Tree  {
     
     static void drawBranches() {
         //  Change the color of the line the robot draws to green    
-
+        rob.setPenColor(Color.CYAN);
         //  Make an int variable called turnAmount and set it to 175
-
+           int turnAmount=175;
         //  Set the robot's angle so it faces right
-
+             rob.turn(90);
         //  Make a for loop that repeats 11 times and put the rest of the code that draws branches inside the for loop
-   
+   for(int i=0; i<12; i++) {
 
             // Move the robot the width of the tree (use the treeWidth variable from the top of this program)
-    		
+    		rob.move(treeWidth);
             // Turn the robot (use the turnAmount variable you made earlier)
-    		
+    		rob.turn(turnAmount);
             // Scale up the treeWidth (    treeWidth *= scale;   )
-    		
+    		treeWidth*=scale;
             // Move the robot the width of a tree again
-    		 
+    		 rob.move(treeWidth);
             // Turn the robot by the current turn amount, but this time turn to the LEFT (hint: negative turns go left)
-    		
+    		rob.turn(-treeWidth);
             // Scale up the treeWidth again (    treeWidth *= scale;   )
-   
+            treeWidth*=scale;
             // Decrease turnAmount by 1
- 
+               turnAmount--;
     	// Make sure your for loop ends here!
+   }
 }
     
     static void drawTrunk() {
